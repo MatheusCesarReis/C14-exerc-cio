1cdcfd957b64c3b5fd33b83a457946bb5bb1573f
Matheus Cesar Reis <cematheus8@gmail.com>
2025-08-18 20:28:42 -0300

    projeto sendo enviado pelo git

---
+++ aprendedo/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+import org.apache.commons.lang3.StringUtils;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String nome = "   Matheus   ";
+
+        // Remove espaços do início e do fim
+        System.out.println(StringUtils.trim(nome)); // "Matheus"
+
+        // Verifica se está vazio ou só com espaços
+        System.out.println(StringUtils.isBlank("   ")); // true
+        }
+}
