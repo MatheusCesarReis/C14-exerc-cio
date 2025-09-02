b7b900b28c18f5331e4db99860527730f128e1a4
tiagogregorio <126029094+tiagogregorio@users.noreply.github.com>
2025-09-02 19:02:50 -0300

    Adicionei exemplos extras de manipulação de strings

--- aprendedo/src/main/java/org/example/Main.java
+++ aprendedo/src/main/java/org/example/Main.java
@@ -13,5 +13,19 @@ public class Main {
 
         // Verifica se está vazio ou só com espaços
         System.out.println(StringUtils.isBlank("   ")); // true
-        }
+
+        // Converte para maiúsculas
+        System.out.println(StringUtils.upperCase(nome1)); // " TIAGO "
+
+// Converte para minúsculas
+        System.out.println(StringUtils.lowerCase(nome1)); // " tiago "
+
+// Remove todos os espaços internos
+        System.out.println(StringUtils.deleteWhitespace(nome1)); // "Tiago"
+
+// Verifica se string é numérica
+        System.out.println(StringUtils.isNumeric("12345")); // true
+        System.out.println(StringUtils.isNumeric("Tiago")); // false
+
+    }
 }
